ed49b41e88450a4f7bcb2acc8a6767c7b544ac4d
Robert <robzidek@umich.edu>
2017-11-03 12:29:43 -0400

    eIMTR controller currently not producing good sim results

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 # sampling time #
-dt = 0.1
+dt = 1.0
 
 # time horizon #
 T = 127.2*60.0
+
+# other parameters #
+mu = 398600
 
 # initial condition #
 r0 = 7778.0
@@ -14,15 +17,12 @@ x0 = np.array([r0,0,np.sqrt(mu/(r0**3))])
 R = np.array([[10**3.5, 0], [0, 10**3.5]])
 S = np.diag([1,1,0,1])
 
-# other parameters #
-mu = 398600
-
 # eIMTR solution #
-x_lin = np.array([8085.1, 0.546/60.0, 0.0, 0.053/60.0])
+x_lin = np.array([8085.1, 0.546/60, 0.0, 0.053/60])
 u_lin = np.array([0.0004, -0.0038])
 psi0 = np.array([-11.93, -54.69, 0, -455009.3])
 A = np.array([ [0,1,0,0],
-               [x_lin[3]**2+mu/x_lin[0]/x_lin[0], 0, 0, 2*x_lin[0]*x_lin[3]],
+               [x_lin[3]**2+2*mu/(x_lin[0]**3), 0, 0, 2*x_lin[0]*x_lin[3]],
                [0,0,0,0],
                [(2.0*x_lin[1]*x_lin[3]-u_lin[1])/(x_lin[0]**2), -2*x_lin[3]/x_lin[0], 0, -2*x_lin[1]/x_lin[0]]])
 B = np.array([[0,0],[1,0],[0,0],[0,1/x_lin[0]]])
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from init import *
 from nonlinear_model import *
 from eIMTR_control import *
+import matplotlib.pyplot as plt
 
 out = f([1,0,0],[1,1])
-print(out)
+
+time = np.arange(0,T,dt)
+
+X = np.zeros((len(time),3))
+U = np.zeros((len(time)-1,2))
+X[0,:]=x0
+
+for i in range(0,len(time)-1):
+    u = eIMTR_control(time[i])
+#    xdot = f(X[i,:],u)
+#    X[i+1,:] = X[i,:] + dt*xdot
+    U[i,:] = u;
+    print(i,'out of',len(time),': r =',X[i,0])
+print('len of time =',len(time[:-1]),'and len(U) =',len(U))
+plt.plot(time[:-1],U[:,0]/3600)
+plt.show()
+    
